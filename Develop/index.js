2c44f86f8d0d2d7304621e5bdbee26ea55fdf81f
Cody Stepp <codystepp691@gmail.com>
2023-12-07 12:36:54 -0600

    used require to import other files and installed inquirere

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,38 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');   
+const inquirer = require('inquirer');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What name would you like to use for your project?",
+    },
+    {
+        type: "input",
+        name: "description ",
+        message: "What is the reason for this project and what is its functionality?",
+    },
+    {
+        type: "checkbox",
+        name: "licence",
+        message: "Please select a licencse applicable to this project.",
+        choices: ["MIT", "none"],
+    },
+    {
+        type: "input",
+        name: "require",
+        message: "Project dependenceies go here",
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "question",
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
